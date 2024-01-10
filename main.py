5f609ae9d28db4fff03ab2d812b97280168c083c
metacalled <46867837+metaacalled@users.noreply.github.com>
2024-01-10 16:28:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import os
 import requests
 import json
 import re
+import time
 from pathlib import Path
 from pwn import log
 from threading import Thread
+
+# Settings
 
 url = 'https://thunderstore.io/api/experimental/package/'
 namespaces = ['notnotnotswipez', 'FlipMods', 'FlipMods', 'FlipMods', 'RugbugRedfern']
 names = ['MoreCompany', 'ReservedItemSlotCore', 'ReservedFlashlightSlot', 'ReservedWalkieSlot', 'Skinwalkers']
 mods_dir = '/tmp/lethalModManager/'
 plugins = f'{mods_dir}BepInEx/plugins/'
+
+# Algo variables.
+threads = []
 
 def downloadMods(namespace, name):
     mod_dir = f'{mods_dir}{name}.zip'
@@ -21,10 +27,10 @@ def downloadMods(namespace, name):
         latest = match.group(1)
         latest = json.loads(latest)
         download_url = latest["download_url"]
-        p = log.progress(f'Downloading {name} from: {download_url}')
+        p = log.progress(f'Downloading {name}... ')
         try:
             rdown = requests.get(download_url, allow_redirects=True)
-            p.success("\n\tDownload successful.")
+            p.success("\tDownload successful.")
         except:
             p.failure(f"Failed to download {name} ({download_url})")
         with open(mod_dir, 'wb') as file:
@@ -45,11 +51,22 @@ def unzipMods():
                 os.rename(os.path.join(mods_dir, dll), os.path.join(plugins, dll))
             except FileExistsError:
                 log.failure(f"{dll} already exists in {plugins}...")
-                
+
+def clearFiles():
+    log.info("Clearing all files.")
+    for file in os.listdir(mods_dir):
+        if file != "BepInEx":
+            os.remove(mods_dir + file)
+
 def main():
     for namespace, name in zip(namespaces, names):
-        Thread(target=downloadMods, args=(namespace, name)).start()
-    unzipMods()
+        t = Thread(target=downloadMods, args=(namespace, name))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join() # Lock program until all threads have finished.
+    unzipMods() # Unzips all downloaded mods and adds them up into BepInEx
+    clearFiles() # Clear all files from the folder except BepInEx
 
 if __name__ == "__main__":
     main()
