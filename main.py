857ab134cea589da0cd038a70d701760afc20382
iand-casa <ian.diaz.7e5@itb.cat>
2024-01-10 16:08:33 +0100

    tempworkin

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ from pwn import log
 url = 'https://thunderstore.io/api/experimental/package/'
 namespaces = ['notnotnotswipez', 'FlipMods', 'FlipMods', 'FlipMods', 'RugbugRedfern']
 names = ['MoreCompany', 'ReservedItemSlotCore', 'ReservedFlashlightSlot', 'ReservedWalkieSlot', 'Skinwalkers']
-home_dir = str(Path.home())
-mods_dir = home_dir + '\\modero\\'
-unzipped = f'{mods_dir}unzipped\\'
-plugins = f'{unzipped}BepInEx\\plugins\\'
+mods_dir = '/tmp/lethalModManager/'
+plugins = f'{mods_dir}BepInEx/plugins/'
 def downloadMods(namespace, name):
-    home_dir = f'{mods_dir}{name}.zip'
+    mod_dir = f'{mods_dir}{name}.zip'
     r = requests.get(f'{url}{namespace}/{name}/')
     if r.status_code == 200:
         match = re.search(r'"latest":\s*({[^}]+})', r.text)
@@ -27,7 +25,7 @@ def downloadMods(namespace, name):
             p.success("\n\tDownload successful.")
         except:
             p.failure(f"Failed to download {name} ({download_url})")
-        with open(home_dir, 'wb') as file:
+        with open(mod_dir, 'wb') as file:
             file.write(rdown.content)
     else:
         print(f"Error, status code: {r.status_code}.")
@@ -37,11 +35,11 @@ def unzipMods():
         if zip.endswith(".zip"):
             zip_path = os.path.join(mods_dir, zip)
             with zipfile.ZipFile(zip_path, 'r') as unzipping:
-                unzipping.extractall(unzipped)
-    for dll in os.listdir(unzipped):
+                unzipping.extractall(mods_dir)
+    for dll in os.listdir(mods_dir):
         if dll.endswith('.dll'):
             try:
-                os.rename(os.path.join(unzipped, dll), os.path.join(plugins, dll))
+                os.rename(os.path.join(mods_dir, dll), os.path.join(plugins, dll))
             except FileExistsError:
                 log.failure(f"{dll} already exists in {plugins}...")
 def main():
