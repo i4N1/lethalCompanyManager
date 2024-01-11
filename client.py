d900a918e5c641a7d5c259d6e1784320c3c57149
metacalled <46867837+metaacalled@users.noreply.github.com>
2024-01-11 10:16:30 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,43 +1,79 @@
-import requests
-import customtkinter
-from customtkinter import filedialog
-from pathlib import Path
+import os
+import time
 import zipfile
+import requests
+from tkinter import *
+from tkinter import filedialog
+from configparser import ConfigParser
 
-default_steam = 'C:\Program Files (x86)\Steam\steamapps\common'
-folder_path = '/home/kali/lethalModManager'
-zip_path = f'{folder_path}/BepInEx.zip'
-url = 'https://cdn.metacalled.tech/uploads/BepInEx.zip'
-#GUI
-app = customtkinter.CTk()
-app.title("lethalModManager")
-app.geometry("500x100")
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
-def browseButton():
-    global folder_path
-    folder_path = filedialog.askdirectory()
-    with open('config.cfg', 'w') as file:
-        file.write(folder_path)
-def updateMods():
-    global folder_path
-    try:
-        with open('config.cfg', 'r') as file:
-            folder_path = file.read()
-    except FileNotFoundError:
-        with open('config.cfg', 'w') as file:
-            file.write(default_steam)
-        print("[-] config.cfg not found, creating it and using default steam location...")
-    r = requests.get(url)
+# Settings
+remote_url = 'https://cdn.metacalled.tech/uploads/BepInEx.zip'
+
+# Algo variables
+
+uiRoot = Tk()
+config_object = ConfigParser()
+
+def clearWidgets():
+    for widget in uiRoot.winfo_children():
+        widget.destroy()
+
+def checkConfig():
+    if not os.path.isfile("config.ini"):
+        with open('config.ini', 'w') as conf:
+            config_object['AppConfig'] = {
+                "gamePath": "None"
+            }
+            config_object.write(conf)
+            conf.close()
+        return ""
+    else:
+        config_object.read("config.ini")
+        return str(config_object["AppConfig"]["gamePath"])
+
+def openFolder():
+    directory = filedialog.askdirectory()
+    with open('config.ini', 'w') as conf:
+        config_object['AppConfig'] = {
+            "gamePath": directory
+        }
+        config_object.write(conf)
+        conf.close()
+    gamePath = checkConfig()
+    pathSelected(gamePath)
+
+def downloadAndExtract(gamePath):
+    clearWidgets()
+    r = requests.get(remote_url)
+    zip_path = gamePath+r"/BepInEx.zip"
     with open(zip_path, 'wb') as file:
         file.write(r.content)
     with zipfile.ZipFile(zip_path, 'r') as unzipping:
-        unzipping.extractall(folder_path)
+        unzipping.extractall(gamePath+r"/BepInEx")
+    os.remove(zip_path)
+    Label(uiRoot, text="Se han actualizado todos los mods!", font=("Verdana",12)).pack(anchor=CENTER)
+    Button(uiRoot, text="OK", command=exit).pack(anchor=CENTER)
 
-#GUI Buttons
-updateButton = customtkinter.CTkButton(app, text="Update mods", command=updateMods)
-updateButton.grid(row=0, column=0, padx=(100,0), pady=10)
-locationButton = customtkinter.CTkButton(app, text="...",fg_color="gray" , command=browseButton)
-locationButton.grid(row=0, column=1, padx=(10,0), pady=10)
+def pathSelected(gamePath):
+    clearWidgets()
+    uiRoot.geometry("500x100")
+    Label(uiRoot, text="Path seleccionado: " + gamePath, font=("Verdana",12)).pack(anchor=CENTER)
+    Label(uiRoot, text="Ya está todo listo, presiona UPDATE para actualizar los mods.", font=("Verdana",10)).pack(anchor=CENTER)
+    Button(uiRoot, text="UPDATE", command=lambda: downloadAndExtract(gamePath)).pack(anchor=CENTER)
 
-app.mainloop()
+def main():
+    uiRoot.title("Lethal Company Manager")
+    uiRoot.resizable(False, False)
+    gamePath = checkConfig()
+    if gamePath == "" or gamePath == "None":
+        uiRoot.geometry("650x75")
+        Label(uiRoot, text="Presiona select y selecciona la carpeta base del juego.", font=("Verdana",12)).pack(anchor=CENTER)
+        Button(uiRoot, text="SELECT", command=openFolder).pack(anchor=CENTER)
+    else:
+        pathSelected(gamePath)
+        
+    
+    
+if __name__ == "__main__":
+    main()
+    uiRoot.mainloop()
